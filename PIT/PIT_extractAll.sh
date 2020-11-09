e009b62504eae59aba23449f416b5742e3b175e9
Charlotte Newton <ccn30@cam.ac.uk>
2020-11-09 10:25:07 +0000

    making PIT main func robust to non-perfect data

--- PIT/PIT_extractAll.sh
+++ PIT/PIT_extractAll.sh
@@ -1,7 +1,6 @@
-#/bin/bash
+#!/bin/bash
 # wrapper script to call PIT_data2table.m
-
-which matlab
+cd slurmoutputs
 pathstem=/lustre/scratch/wbic-beta/ccn30/ENCRYPT
 
 ## separate txt file with subject and date IDs
@@ -9,13 +8,20 @@ pathstem=/lustre/scratch/wbic-beta/ccn30/ENCRYPT
 #!mysubjs=${pathstem}/testsubjcode.txt
 mysubjs=${pathstem}/ENCRYPT_MasterCBcodes.txt
 
-xmlDir='/lustre/scratch/wbic-beta/ccn30/ENCRYPT/task_data/PIT/raw_data'
+## set paths
+
+xmlDir=/lustre/scratch/wbic-beta/ccn30/ENCRYPT/task_data/PIT/raw_data
+resultsDir=/lustre/scratch/wbic-beta/ccn30/ENCRYPT/results/PIT
+scriptDir=${pathstem}/scripts/PIT
+submit=${scriptDir}/PIT_submit.sh
+run=${scriptDir}/PIT_raw2table_run.sh
+func=${scriptDir}/PIT_raw2table.m
 
 for CBcode in `cat $mysubjs`
 do
 
 echo "******** starting $CBcode ********"
 
-#"$MATLABPATH"/bin/matlab.nowrap -nodesktop -nosplash -nodisplay -r "try, PIT_data2table('${CBcode}',${xmlDir}); end ; quit"
+sbatch ${submit} ${run} ${func} ${scriptDir} ${xmlDir} ${resultsDir} ${CBcode}
 
 done
---
+++ PIT/PIT_raw2table_run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# arguments from PIT_submit.sh
+
+func=${1}
+xmlDir=${2}
+resultsDir=${3}
+CBcode=${4}
+pwd
+
+# matlab call
+
+matlab -nodesktop -nosplash -nodisplay <<EOF
+[pa,af,~]=fileparts('${func}');
+addpath(pa);
+addpath(pwd);
+dofunc=sprintf('%s(%s,%s,%s)',af,'''${CBcode}''','''${xmlDir}''','''${resultsDir}''');
+disp(['Matlab submitting the following command: ' dofunc])
+eval(dofunc)
+;exit
+EOF
