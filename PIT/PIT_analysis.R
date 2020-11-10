dc5d224dd34cd5ae4d136e54992fd1a78002d362
Charlotte Newton <ccn30@cam.ac.uk>
2020-11-10 18:24:09 +0000

    playing around with PIT data

--- PIT/PIT_analysis.R
+++ PIT/PIT_analysis.R
@@ -1,1 +1,31 @@
 ## PIT analysis
+require(ggplot2)
+require(dplyr)
+require(tibble)
+require(readr)
+require(tidyr)
+require(RColorBrewer)
+require(ggthemes)
+require(lme4)
+
+resultsDir <- '/lustre/scratch/wbic-beta/ccn30/ENCRYPT/results/PIT'
+scriptDir <- '/lustre/scratch/wbic-beta/ccn30/ENCRYPT/scripts/PIT/slurmoutputs'
+setwd(scriptDir)
+csvFiles <- list.files(resultsDir,pattern="*.csv",full.names=1)
+
+nFiles <- length(csvFiles)
+
+# make master table 
+
+for(i in 1:nFiles)
+{
+  output[i] <- paste("csv",i,sep="")
+  assign(output[i], as_tibble(read.csv(csvFiles[i])))
+}
+for(i in 0:nFiles)
+{
+  test <- paste(as.name(output[i]))
+  PITall <- bind_rows(as.name(output[i+1]))
+}
+PITall <- bind_rows(csv1,csv2,csv3,csv4)
+
