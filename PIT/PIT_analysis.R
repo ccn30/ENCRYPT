09f7f7ccc3694cfccfd97d792f2082e26d085927
Charlotte Newton <ccn30@cam.ac.uk>
2020-11-10 18:57:23 +0000

    discovered elegant coding in R

--- PIT/PIT_analysis.R
+++ PIT/PIT_analysis.R
@@ -13,19 +13,9 @@ scriptDir <- '/lustre/scratch/wbic-beta/ccn30/ENCRYPT/scripts/PIT/slurmoutputs'
 setwd(scriptDir)
 csvFiles <- list.files(resultsDir,pattern="*.csv",full.names=1)
 
-nFiles <- length(csvFiles)
+# make master table
+tibblesList=lapply(csvFiles,read_csv)
+PITall <- bind_rows(tibblesList)
 
-# make master table 
 
-for(i in 1:nFiles)
-{
-  output[i] <- paste("csv",i,sep="")
-  assign(output[i], as_tibble(read.csv(csvFiles[i])))
-}
-for(i in 0:nFiles)
-{
-  test <- paste(as.name(output[i]))
-  PITall <- bind_rows(as.name(output[i+1]))
-}
-PITall <- bind_rows(csv1,csv2,csv3,csv4)
 
